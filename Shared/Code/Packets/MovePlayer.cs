5e5c36c958c8f599096ecf22c35c72f9bd831751
cwtrucks <cwtrucks@ncsu.edu>
2024-08-16 19:53:42 -0400

    Free camera mode with LCtrl

--- Shared/Code/Packets/MovePlayer.cs
+++ Shared/Code/Packets/MovePlayer.cs
@@ -1,8 +1,12 @@
 using LiteNetLib.Utils;
+using System.Diagnostics;
 namespace Shared.Code.Packets;
 
 public sealed class MovePlayer : IPacket {
     public Vector2Int Position { get; set; }
+
+    public Vector2Int AddToPosition { get; set; }
+
     public ushort TickNum { get; init; }
 
     public int SourceId { get; init; }
@@ -16,7 +20,8 @@ public sealed class MovePlayer : IPacket {
     }
 
     public void Process(WorldState worldState) {
-        worldState.PlayerPositions[SourceId].Position = Position + new Vector2Int(256, 128);
+        //worldState.PlayerPositions[SourceId].Position = Position + new Vector2Int(256, 128);
+        worldState.PlayerPositions[SourceId].Position += AddToPosition;
     }
 
     public PacketType GetPacketType() => PacketType.MovePlayer;
